4c270b117ecebda96f9ff1fbbcd336803c5ea667
sari7happy <sari7happy@gmail.com>
2022-09-26 17:59:39 +0900

    ”小数点型の調べ方var_dump”

--- main.php
+++ main.php
@@ -1,10 +1,10 @@
 <?php 
-$return =1;
-$class=8;
+    $num01 =123;
+    $num02 =1.23;
 
+    echo gettype($num01)."\n";
+    echo gettype($num02)."\n";
+    var_dump($num02)."\n";
 
-echo $return."\n";
-echo $class."\n";
-
-
-// phpは予約語を変数にするのはしないほうが良い。エラーを引き起こすから。class,returnなど
+//phpの数値型には整数型のinteger型、小数点のfloat型、double型（小数点以下の桁数を多く使える。）
+//小数点型を調べるのはvar_dumpを使う
