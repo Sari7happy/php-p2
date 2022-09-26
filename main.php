720fee897067950f43ba30d5c8243a3875441543
sari7happy <sari7happy@gmail.com>
2022-09-26 11:20:33 +0900

    ”大文字、小文字の区別”

--- main.php
+++ main.php
@@ -1,11 +1,10 @@
 <?php 
-$num =1;
-$num01=2;
-$num_01=3;
-
-echo $num."\n";
-echo $num01."\n";
-echo $num_01."\n";
+$NUM =7;
+$Num=8;
 
 
-// phpはアルファベット、数字、_がつかえる。この記号以外は使えない
+echo $NUM."\n";
+echo $Num."\n";
+
+
+// phpは大文字、小文字区別できる。
