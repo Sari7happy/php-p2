a39fad3f72aea654162e73607aadafd2cd7fe593
sari7happy <sari7happy@gmail.com>
2022-10-04 23:36:41 +0900

    ”追加SQL”

--- my.sql
+++ my.sql
@@ -1,17 +1,19 @@
 SELECT * FROM  players;
 --プレイヤーのテーブルから取り出す
-SELECT name,level FROM players;
+SELECT name, level FROM players;
 -- 特定のカラムだけ取り出すのはカンマで区切る
 
 SELECT * FROM players WHERE level >= 7;
 -- レベル7以上のみ取り出す
-SELECT * FROM players WHERE level>=7AND job_id<>6;
+SELECT * FROM players WHERE level>=7 AND job_id <> 6;
 -- 条件を組み合わせるとき7以上で6も等しくないとき
-SELECT name,level FROM players WHERE level >=7;
+SELECT name, level FROM players WHERE level >=7;
 -- レベル7以上の名前とレベル表示
-SELECT COUNT(*)FROM players;
+SELECT COUNT(*) FROM players;
 -- カウント機能
-SELECT *FROM players ORDER BY LEVEL;
+SELECT * FROM players ORDER BY LEVEL;
 -- レベル順に並び替え
-SELECT *FROM players ORDER BY LEVEL DESC;
+SELECT * FROM players ORDER BY LEVEL DESC;
 -- DESCでレベル高い方から並び替え
+INSERT into players(id,name,level,job_id) VALUES(11,"霧島1号",1,1);
+-- 新しくメンバーを追加する方法
