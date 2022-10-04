334c0ddc931870769f50bf23e6ed7ca2f459d5e1
sari7happy <sari7happy@gmail.com>
2022-10-04 23:16:23 +0900

    ”SQLカウント機能”

--- my.sql
+++ my.sql
@@ -9,3 +9,5 @@ SELECT * FROM players WHERE level>=7AND job_id<>6;
 -- 条件を組み合わせるとき7以上で6も等しくないとき
 SELECT name,level FROM players WHERE level >=7;
 -- レベル7以上の名前とレベル表示
+SELECT COUNT(*)FROM players;
+-- カウント機能
