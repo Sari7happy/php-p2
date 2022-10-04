b346227857af3c967cc4d66049f2819241f7086e
sari7happy <sari7happy@gmail.com>
2022-10-04 22:50:39 +0900

    ”複数の条件の組み合わせSQL #

--- my.sql
+++ my.sql
@@ -1,4 +1,9 @@
 SELECT * FROM  players;
 --プレイヤーのテーブルから取り出す
 SELECT name,level FROM players;
-特定のカラムだけ取り出すのはカンマで区切る
+-- 特定のカラムだけ取り出すのはカンマで区切る
+
+SELECT * FROM players WHERE level >= 7;
+-- レベル7以上のみ取り出す
+SELECT * FROM players WHERE level>=7AND job_id<>6;
+-- 条件を組み合わせるとき7以上で6も等しくないとき
