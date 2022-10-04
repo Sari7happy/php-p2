0f038fb3befcd345e2127d4150383bf4a4853e9a
sari7happy <sari7happy@gmail.com>
2022-10-04 15:18:18 +0900

    ”PHPにhtml入力”

--- main3.php
+++ main3.php
@@ -1,10 +1,6 @@
-
+<b>
+    <!-- 太文字はbタグ -->
 <?php
-    $greeting = "Hello";
-    // echo $greeting ."<br>";
-    // echo $greeting ."<br>";
-    // echo $greeting ."<br>";
-    for($i =0; $i <10;$i++){
-        echo $greeting ."<br>";
-    }
+    echo "hello saru";
 ?>
+</b>
