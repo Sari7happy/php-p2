10243f747a3c80136b16eda9b6bb9d3aa0853af5
sari7happy <sari7happy@gmail.com>
2022-10-04 10:55:55 +0900

    ”変数連結”

--- main2.php
+++ main2.php
@@ -1,5 +1,11 @@
 <?php
-    $greeting = "hello world";
+    $greet = "hello smil<br>";
     // 変数を代入
-    echo $greeting;
+    echo $greet;
+    echo $greet;
+    $name = "good";
+    echo $name ."morning";
+    // 繋ぐときはドットで繋ぐ
+
+
 ?>
